3a54020019538020e3a96ef13faaf7d560e3447e
Lucas Melin <lucas.melin@gmail.com>
2022-03-29 20:04:46 -0400

    Implement cyoa html handler

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/lucasmelin/gophercises/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 8000, "the port to start the web application on")
 	filenamePtr := flag.String("file", "gopher.json", "a JSON file with a Choose Your Own Adventure story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s\n", *filenamePtr)
@@ -21,7 +24,11 @@ func main() {
 	if err != nil {
 		exit(fmt.Sprintf("Failed to parse the story: %s", *filenamePtr))
 	}
-	fmt.Printf("%+v\n", story)
+	handler := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port %d\n", *port)
+	// This will log if the server returns a value, which shouldn't normally happen
+	// since the server should run indefinitely
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
 }
 
 func exit(msg string) {
--- cyoa/story.go
+++ cyoa/story.go
@@ -2,8 +2,47 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Options}}
+      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+    </ul>
+  </body>
+</html>`
+
+func NewHandler(story Story) http.Handler {
+	return handler{story}
+}
+
+type handler struct {
+	story Story
+}
+
+func (h handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	tpl := template.Must(template.New("").Parse(defaultHandlerTemplate))
+	err := tpl.Execute(writer, h.story["intro"])
+	if err != nil {
+		panic(err)
+	}
+
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	decoder := json.NewDecoder(r)
